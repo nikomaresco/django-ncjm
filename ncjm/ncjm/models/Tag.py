b2d985bc5f80a8cb3e79c46a5cea40644c529079
nmaresco <wizzahd@gmail.com>
2024-11-30 17:47:19 -0500

    tag_text now a CharField instead of TextField. added clean() and save() methods to ensure uniqueness and lowercase-ness.

--- ncjm/ncjm/models/Tag.py
+++ ncjm/ncjm/models/Tag.py
@@ -1,25 +1,48 @@
 from django.db import models
-from ncjm.models import Joke
+from django.core.exceptions import ValidationError
 
 class Tag(models.Model):
-    joke = models.ForeignKey(
-        Joke,
-        help_text="The joke to which this tag is attached.",
-        on_delete=models.CASCADE,
+    created_at = models.DateTimeField(
+        help_text="The date and time the tag was created.",
+        auto_now_add=True,
     )
 
-    tag_text = models.TextField(
+    tag_text = models.CharField(
         help_text="The tag text.",
-        unique=True,  # Enforce unique constraint on tag_text
+        max_length=100,
+        unique=True,
+    )
+
+    ext_id = models.IntegerField(
+        help_text="The external ID of the tag.",
+        null=True,
+        blank=True,
     )
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["tag_text", "joke"],
-                name="unique_tag_text_joke"
+                fields=["tag_text"],
+                name="unique_tag_text"
             )
         ]
 
     def __str__(self):
         return self.tag_text
+
+    def clean(self):
+        """
+        Validate the tag text for case-insensitive uniqueness.
+        """
+        if Tag.objects.filter(tag_text__iexact=self.tag_text) \
+            .exclude(pk=self.pk).exists():
+            raise ValidationError(f"Tag with text '{self.tag_text}' already exists.")
+
+
+    def save(self, *args, **kwargs):
+        """
+        Save the tag text in lowercase.
+        """
+        self.tag_text = self.tag_text.lower()
+        self.clean()
+        super().save(*args, **kwargs)
