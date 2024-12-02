710c2a61801ba16a4790d514cc45e298c2482791
nmaresco <wizzahd@gmail.com>
2024-12-02 15:42:53 -0500

    fixed quotes

--- ncjm/ncjm_site/apps.py
+++ ncjm/ncjm_site/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NcjmSiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ncjm_site'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ncjm_site"
