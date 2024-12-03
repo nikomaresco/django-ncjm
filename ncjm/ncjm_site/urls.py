234818f95883a3476af2656b71077dfd3cdfee2a
nmaresco <wizzahd@gmail.com>
2024-12-03 10:44:20 -0500

    added other frontend urls

--- ncjm/ncjm_site/urls.py
+++ ncjm/ncjm_site/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-
-
+    path("search/", views.search, name="search"),
+    path("add-a-joke/", views.add_joke, name="add_joke"),
+    path("<int:joke_id>/", views.index, name="joke_by_id"),
+    path("<slug:joke_slug>/", views.index, name="joke_by_slug"),
 ]
