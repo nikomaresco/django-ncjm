f7087ba8f498447cdd2f2f706086776dee7fe608
nmaresco <wizzahd@gmail.com>
2024-12-03 22:03:49 -0500

    add react/ endpoint

--- ncjm/ncjm_site/site_urls.py
+++ ncjm/ncjm_site/site_urls.py
@@ -5,8 +5,11 @@ from . import site_views
 
 urlpatterns = [
     path("", site_views.index, name="index"),
+
     path("search/", site_views.search, name="search"),
     path("add-a-joke/", site_views.add_joke, name="add_joke"),
+    path("react/", site_views.add_reaction, name="add_reaction"),
+
     path("<int:joke_id>/", site_views.index, name="joke_by_id"),
     path("<slug:joke_slug>/", site_views.index, name="joke_by_slug"),
 ]
