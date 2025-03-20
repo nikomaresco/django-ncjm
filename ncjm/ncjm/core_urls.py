ba5b9a19335457a6211e08483194ced79ce2be72
Niko Maresco <niko@page1llc.com>
2025-03-20 19:24:50 -0400

    changed admin path to ncjm-admin

--- ncjm/ncjm/core_urls.py
+++ ncjm/ncjm/core_urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("ncjm-admin/", admin.site.urls),
     path("", include("ncjm_site.site_urls")),
 ]
 
