8567a2aeaab54a4f5d3417879d1cdbf83f2aa8ba
Niko Maresco <niko@page1llc.com>
2025-12-26 20:59:31 -0700

    feat: add common aliases to docker image

---
+++ .docker/dockerfile-aliases.sh
@@ -0,0 +1,24 @@
+# general use commands
+alias ll='ls -la'
+
+alias cd..='cd ..'
+alias cd...='cd ../..'
+alias cd....='cd ../../..'
+
+# python aliases
+alias py='python3'
+alias python='python3'
+
+# django aliases
+alias sp='python3 manage.py shell_plus'
+alias manage='python3 manage.py'
+
+# celery aliases
+# alias cel='celery -A ddpbroker'
+# alias tasks='celery -A ddpbroker list tasks'
+# alias activetasks='celery -A ddpbroker inspect active'
+# alias scheduledtasks='celery -A ddpbroker inspect scheduled'
+# alias reservedtasks='celery -A ddpbroker inspect reserved'
+# alias revokedtasks='celery -A ddpbroker inspect revoked'
+# alias registeredtasks='celery -A ddpbroker inspect registered'
+# alias stats='celery -A ddpbroker inspect stats'
