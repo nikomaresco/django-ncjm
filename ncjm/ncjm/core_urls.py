0cbfb055283ba2b8a6b1bb007f451ecef407ca59
Niko Maresco <niko@page1llc.com>
2025-12-26 21:00:18 -0700

    feat: solve issue with staticfiles 404 in dev feat: add support for django debug toolbar

--- ncjm/ncjm/core_urls.py
+++ ncjm/ncjm/core_urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
 urlpatterns = [
@@ -14,7 +15,14 @@ if settings.NCJM_API_ENABLED:
         path("oauth2/", include("oauth2_provider.urls", namespace="oauth2_provider")),
     ]
 
-urlpatterns += static(
-    settings.STATIC_URL,
-    document_root=settings.STATIC_ROOT
-)
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path("_debug__/", include(debug_toolbar.urls)),
+    ]
+    urlpatterns += staticfiles_urlpatterns()
+else:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
